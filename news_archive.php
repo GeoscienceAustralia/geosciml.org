a58674691b2e58695bc49430320cca0665cdd4c1
Pete <petess@gmail.com>
2018-05-17 15:35:17 +1000

    changed pages to use html

--- news_archive.php
+++ news_archive.php
@@ -8,12 +8,12 @@
         <!--?php include $PROJECT_HOME_DIR . 'theme/right_menu.inc'; ?-->
 
         <?php
+            include 'htmlpages/news_archive.html'
             // read file contents from Markdown (.md) file
-            $contents = file_get_contents($PROJECT_HOME_DIR . 'pages/news_archive.md');
-            $Parsedown = new Parsedown();
-            echo $Parsedown->text($contents);
+            // $contents = file_get_contents($PROJECT_HOME_DIR . 'pages/news_archive.md');
+            // $Parsedown = new Parsedown();
+            // echo $Parsedown->text($contents);
         ?>
-	
     </div><!-- #content-container -->
 
 <?php include $PROJECT_HOME_DIR . 'theme/footer.inc'; ?>
